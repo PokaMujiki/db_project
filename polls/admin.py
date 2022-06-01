36cd34cfc29a2a52c564911555682ece225cf885
Alexandr Baranov <baranovau216@gmail.com>
2022-06-02 04:39:14 +0700

    better crud interfaces

--- polls/admin.py
+++ polls/admin.py
@@ -4,28 +4,112 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(TradePointType)
-admin.site.register(TradePoint)
-admin.site.register(SomeStore)
-admin.site.register(DepartmentStore)
-admin.site.register(Employee)
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Receipt)
+
+class SoldProductInline(admin.TabularInline):
+    model = SoldProduct
+
+
+@admin.register(TradePointType)
+class TradePointTypeDisplay(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+@admin.register(TradePoint)
+class TradePointDisplay(admin.ModelAdmin):
+    list_display = ("name", "point_type", "point_size", "rent_payment", "utilities_payment", "point_counter_amount")
+    inlines = [SoldProductInline]
+
+@admin.register(Employee)
+class EmployeeDisplay(admin.ModelAdmin):
+    list_display = ("name", "salary", "working_point")
+
+
+@admin.register(Customer)
+class CustomerDisplay(admin.ModelAdmin):
+    list_display = ("name", "info")
+
+
+@admin.register(Product)
+class ProductDisplay(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+
+@admin.register(SomeStore)
+class SomeStoreDisplay(admin.ModelAdmin):
+    list_display = ("trade_point",)
+
+
+@admin.register(DepartmentStore)
+class DepartmentStoreDisplay(admin.ModelAdmin):
+    list_display = ("trade_point",)
+
+
+# todo fields
+@admin.register(Section)
+class SectionDisplay(admin.ModelAdmin):
+    list_display = ("trade_point", "section_number", "floor", "section_manager")
+
+
+@admin.register(Hall)
+class HallDisplay(admin.ModelAdmin):
+    list_display = ("trade_point", "hall_number", "employees_number")
+
+
+@admin.register(Request)
+class RequestDisplay(admin.ModelAdmin):
+    list_display = ("trade_point", "date")
+
+
+class DistributorProductInline(admin.TabularInline):
+    model = DistributorProduct
+
+
+@admin.register(Distributor)
+class DistributorDisplay(admin.ModelAdmin):
+    list_display = ("name", "contact", "rating")
+    inlines = [DistributorProductInline]
+
+
+@admin.register(ProductsOrder)
+class ProductsOrderDisplay(admin.ModelAdmin):
+    list_display = ("date", )
+
+
+class ReceiptItemInline(admin.TabularInline):
+    model = ReceiptItem
+
+
+@admin.register(Receipt)
+class ReceiptDisplay(admin.ModelAdmin):
+    list_display = ("date", "trade_point", "customer", "employee")
+    inlines = [ReceiptItemInline]
 
 
 @admin.register(ReceiptItem)
-class ReceiptItemAdmin(admin.ModelAdmin):
-    list_display = ("receipt", "product", "amount")
+class ReceiptItemDisplay(admin.ModelAdmin):
+    list_display = ("receipt", "product", "price", "amount")
 
 
-admin.site.register(Section)
-admin.site.register(Hall)
-admin.site.register(SoldProduct)
-admin.site.register(Distributor)
-admin.site.register(Request)
-admin.site.register(RequestItem)
-admin.site.register(DistributorProduct)
-admin.site.register(ProductsOrder)
-admin.site.register(ProductOrderItem)
-admin.site.register(RequestOrder)
+@admin.register(SoldProduct)
+class SoldProductDisplay(admin.ModelAdmin):
+    list_display = ("trade_point", "product", "in_stock", "price")
+
+
+@admin.register(RequestItem)
+class RequestItemDisplay(admin.ModelAdmin):
+    list_display = ("request", "product", "amount")
+
+
+@admin.register(DistributorProduct)
+class DistributorProductDisplay(admin.ModelAdmin):
+    list_display = ("distributor", "product", "price", "offer_start_date", "offer_end_date")
+
+
+@admin.register(ProductOrderItem)
+class ProductsOrderItemDisplay(admin.ModelAdmin):
+    list_display = ("products_order", "distributor_product", "amount", "price")
+
+
+@admin.register(RequestOrder)
+class RequestOrderDisplay(admin.ModelAdmin):
+    list_display = ("request", "order")
--- polls/models.py
+++ polls/models.py
@@ -82,7 +82,7 @@ class Customer(models.Model):
     info = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        if self.info is not None:
+        if self.info is not None and self.info != "":
             return self.name + " | " + self.info
         return self.name
 
@@ -94,7 +94,7 @@ class Product(models.Model):
                                           through_fields=('product', 'trade_point'), related_name='sold_products')
 
     def __str__(self):
-        if self.description is not None:
+        if self.description is not None and self.description != "":
             return self.name + " | " + self.description
         return self.name
 
@@ -231,7 +231,7 @@ class DistributorProduct(models.Model):
 
     def __str__(self):
         return self.distributor.name + " | " + self.product.name + " | price: " + str(self.price) +\
-               " | " + str(self.offer_start_date) + " | is active: " + (self.offer_end_date is None)
+               " | " + str(self.offer_start_date) + " | is active: " + str(self.offer_end_date is None)
 
 
 class ProductsOrder(models.Model):
